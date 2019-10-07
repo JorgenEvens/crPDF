09b51082297334c10cc93973b194047b6cf6f5f0
Jorgen Evens <jorgen@evens.eu>
2019-10-07 22:30:07 +0200

    [stdin] add support for `-`

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ module.exports = async function toPDF(config) {
 
     let url = input;
     if (!/^[a-z0-9-_]+:\/\//i.test(input + '')) {
-        const html = input ? fs.readFileSync(input).toString('utf8') : await readStdin();
+        const html = (input && input != '-') ? fs.readFileSync(input).toString('utf8') : await readStdin();
         url = `data:text/html;base64,${new Buffer(html, 'utf8').toString('base64')}`;
     }
 
